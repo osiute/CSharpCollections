f929032d4bda08c3be8150e8c89fcf72059fd51a
osiute <osiute.programming@gmail.com>
2023-10-06 13:45:56 -0700

    Add DynamicArray class (prime version). Now you can create dynamic array; get / set value, using index; push and pop last element. A dynamic array is based on a static.

---
+++ CSharpCollections/DynamicArray.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCollections
+{
+    public class DynamicArray<T>
+    {
+        private T[] _internalArray;
+        public int Size { get; private set; }
+
+        public DynamicArray(params T[] values)
+        {
+            Size = values.Length;
+            if (values.Length == 0)
+            {
+                _CreateNewInternalArray();
+                return;
+            }
+            _internalArray = values;
+        }
+
+
+        public T this[int index]
+        {
+            get
+            {
+                _CheckIndexOrError(ref index);
+                return _internalArray[index];
+            }
+            set
+            {
+                _CheckIndexOrError(ref index);
+                _internalArray[index] = value;
+            }
+        }
+        public void PushBack(T value)
+        {
+            if (_IsInternalFull())
+            {
+                _ExpandInternalArray();
+            }
+            _internalArray[Size++] = value;
+        }
+
+        public T PopBack()
+        {
+            if (IsEmpty())
+            {
+                throw new Exception("Delete from empty dynamic array attempt.");
+            }
+            return _internalArray[--Size];
+        }
+
+        public bool IsEmpty()
+        {
+            return Size == 0;
+        }
+
+        private bool _IsInternalFull()
+        {
+            return Size == _internalArray.Length;
+        }
+
+        private void _CheckIndexOrError(ref int index)
+        {
+            if (index < 0) index = Size + index;
+            if (index < 0 || index >= Size)
+                throw new IndexOutOfRangeException($"Dynamic array index out of range.({index})");
+        }
+
+        private void _ExpandInternalArray()
+        {
+            var newArray = new T[_internalArray.Length * 3];
+            for (int i = 0; i < Size; ++i)
+            {
+                newArray[i] = _internalArray[i];
+            }
+            _internalArray = newArray;
+        }
+
+        private void _CreateNewInternalArray()
+        {
+            _internalArray = new T[10];
+        }
+    }
+}
---
+++ Tets/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpCollections;
+
+namespace Tets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stringArray = Console.ReadLine().Split();
+            var arr = new DynamicArray<string>(stringArray);
+            for (int i = 1; i <= arr.Size; ++i)
+            {
+                Console.Write(arr[-i] + "; ");
+            }
+        }
+    }
+}
